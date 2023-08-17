0b2f970084cfc8f65b7fec5978e91230766808e4
BechirBerraies <136343630+BechirBerraies@users.noreply.github.com>
2023-08-17 19:34:53 +0100

    Routes

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -9,3 +9,19 @@ bcrypt = Bcrypt(app)
 @app.route('/')
 def index():
     return render_template("home.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template("registration_form.html")
+
+@app.route('/user')
+def user():
+    return render_template("user.html")
+
+@app.route('/register2')
+def register2():
+    return render_template("register2.html")
