34da8df957b218aaac440180f7cf338ce5bac2f5
BechirBerraies <136343630+BechirBerraies@users.noreply.github.com>
2023-08-22 17:31:48 +0100

    hello

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,102 +1,117 @@
-<<<<<<< HEAD
-=======
 from flask_app import app
-from flask import render_template , request, redirect,session, flash
+from flask import render_template, request, redirect, session, flash
 from flask_app.models.user import User
+from flask_app.models.blood_type import Blood_type
+from flask_app.models.demand import Demand
 
-from flask_bcrypt import Bcrypt        
+from flask_bcrypt import Bcrypt
+
 bcrypt = Bcrypt(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return render_template("home.html")
 
-@app.route('/login')
+
+@app.route("/login")
+def form_register():
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
 def login():
     print(request.form)
-    user_from_db = User.get_by_email({'email':request.form['email']})
+    user_from_db = User.get_by_email({"email": request.form["email"]})
     print(user_from_db)
     if user_from_db:
-        if not bcrypt.check_password_hash(user_from_db.password, request.form['password']):
+        if not bcrypt.check_password_hash(
+            user_from_db.password, request.form["password"]
+        ):
             # if we get False after checking the password
-            flash("Wrong Password !!!","login")
-            return redirect('/home.html')
-        session['user_id'] = user_from_db.id
-        return redirect('/login.html')
-    flash("Wrong email !!!!","login")
-    return redirect('/home.html')
+            flash("Wrong Password !!!", "login")
+            return redirect("/home.html")
+        session["user_id"] = user_from_db.id
+        return redirect("/login.html")
+    flash("Wrong email !!!!", "login")
+    return redirect("/home.html")
 
 
-@app.route('/user_dashboard')
-def dashboard():
-    if 'user_id' not in session:
-        return redirect('/home.html')
-    logged_user = User.get_by_id({'id':session['user_id']})
-    return render_template("login.html",logged_user=user)
+# @app.route('/user')
+# def dashboard():
+#     if 'user_id' not in session:
+#         return redirect('/home.html')
+#     logged_user = User.get_by_id({'id':session['user_id']})
+#     return render_template("login.html",logged_user=user)
 
-@app.route('/register', methods=["POST"])
+
+@app.route("/register", methods=["POST"])
 def register():
-    print('*****REG************',request.form)
     if User.validate_register(request.form):
-         pw_hash = bcrypt.generate_password_hash(request.form['password'])
-         print("Password : ", request.form['password'])
-         print("Hashed Password : ", pw_hash)
-         data_dict = {
-             **request.form,
-             'password':pw_hash
-         }
-         user_id = User.create(data_dict)
-         session['user_id'] = user_id
-         return redirect('/register2')
-    return redirect('/register')
+        pw_hash = bcrypt.generate_password_hash(request.form["password"])
+        print("Password : ", request.form["password"])
+        print("Hashed Password : ", pw_hash)
+        data_dict = {**request.form, "password": pw_hash}
+        session["pw_hash"] = pw_hash
+        session["first_name"] = request.form["first_name"]
+        session["last_name"] = request.form["last_name"]
+        session["email"] = request.form["email"]
+        #  session['user_id'] = user_id
+        return redirect("/register2")
+    return redirect("/register")
 
-@app.route('/user')
+
+@app.route("/register")
+def form():
+    return render_template("register.html")
+
+
+@app.route("/user")
 def user():
     every_demand = Demand.get_all_demands_with_hospitals()
-    print(every_demand[0].hospital)
-    if 'user_id' not in session:
-        return redirect('/home.html')
-    logged_user = User.get_by_id({'id':session['user_id']})
-    return render_template("user.html", every_demand = every_demand )
+    # if 'user_id' not in session:
+    #     return redirect('/home.html')
+    # logged_user = User.get_by_id({'id':session['user_id']})
+    data = User.get_users_with_blood_type()
+    print(data)
+    return render_template("user.html", every_demand=every_demand)
 
-@app.route('/second_register')
+
+@app.route("/second_register")
 def form11():
     all_blood = Blood_type.get_all_types()
     return render_template("register2.html", all_blood=all_blood)
 
-@app.route('/register2', methods=['POST'])
+
+@app.route("/register2", methods=["POST"])
 def register2():
-    data={
-        'blood_type_id':request.form['blood_type_id'],
-        'first_name':session['first_name'],
-        'last_name': session['last_name'],
-        'email': session['email'],
-        'password':session['pw_hash'],
-        'phone':request.form['phone'],
-        'CIN':request.form['CIN'],
-        'date_birth':request.form['date'],
-        'address':request.form['address']
+    data = {
+        "blood_type_id": request.form["blood_type_id"],
+        "first_name": session["first_name"],
+        "last_name": session["last_name"],
+        "email": session["email"],
+        "password": session["pw_hash"],
+        "phone": request.form["phone"],
+        "CIN": request.form["CIN"],
+        "date_birth": request.form["date"],
+        "address": request.form["address"],
     }
+    print(request.form)
     user_id = User.create(data)
     return redirect("/user")
 
 
-
-
-
-
-
-@app.route('/my_account')
+@app.route("/my_account")
 def my_account():
     return render_template("my_account.html")
 
-@app.route('/hospital_dashboard')
+
+@app.route("/hospital_dashboard")
 def hospital_dashboard():
     return render_template("Hospital.html")
 
->>>>>>> 6c48d587cf627c2fd059fc36c635e0083d355370
 
-@app.route('/blood_request')
+@app.route("/blood_request")
 def blood_request():
     every = Demand.get_all_demands_with_hospitals()
-    return render_template("blood_request.html",every = every)
+    return render_template("blood_request.html", every=every)
--- flask_app/models/blood_type.py
+++ flask_app/models/blood_type.py
@@ -1,6 +1,4 @@
 from flask_app.config.mysqlconnection import MySQLConnection
-from flask import flash
-from flask_app import DATABASE_NAME
 
 b_comp = {"O+": ['O+', 'A+', 'B+', 'AB+'],
           "A-": ['A-', 'A+', 'AB-', 'AB+'],
@@ -30,5 +28,24 @@ class Blood_type:
             type = cls(row)
             all_types.append(type)
         return all_types
+    
+    @staticmethod
+    def check_compatibility(donor_btype, recipient_btype):
+        for blood in b_comp[donor_btype]:
+            if blood == recipient_btype:
+                return True
+            return False
+    
+    
+
+
+    # @staticmethod
+    # def check_compatibility(donor_btype, recipient_btype):
+	# 	Demand.get_all_demands_with_hospitals()
+	# 	User.get_users_with_blood_type()
+	# 	for blood in b_comp[donor_btype]:
+	# 			if blood == recipient_btype:
+	# 				return True
+	# 		return False
 
 
