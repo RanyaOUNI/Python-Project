edc6e426a8c0d1ce685e2117e6fc7a1ee154ae68
omardaly <136573570+omardaly@users.noreply.github.com>
2023-08-22 16:00:56 +0100

    call

--- flask_app/models/blood_type.py
+++ flask_app/models/blood_type.py
@@ -5,9 +5,8 @@ from flask_app import DATABASE_NAME
 
 class Blood_type:
     def __init__(self, data_dict):
-        self.id = data_dict['id']
-        self.type = data_dict['type']
-
+        self.id = data_dict["id"]
+        self.type = data_dict["type"]
 
     @classmethod
     def get_all():
@@ -17,10 +16,8 @@ class Blood_type:
     def get_all_types(cls):
         query = """SELECT * FROM blood_type;"""
         result = MySQLConnection(DATABASE_NAME).query_db(query)
-        all_types =[]
+        all_types = []
         for row in result:
             type = cls(row)
             all_types.append(type)
         return all_types
-
-
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -3,95 +3,90 @@ from flask_app.config.mysqlconnection import MySQLConnection
 from flask import flash
 
 
-
-
 import re
 
-EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
-PASSWORD_REGEX = re.compile('\d.*[A-Z]|[A-Z].*\d')
+EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$")
+PASSWORD_REGEX = re.compile("\d.*[A-Z]|[A-Z].*\d")
 
-class User :
+
+class User:
     def __init__(self, data_dict):
-        self.id = data_dict['id']
-        self.blood_type_id = ['blood_type_id']
-        self.first_name = data_dict['first_name']
-        self.last_name = data_dict['last_name']
-        self.email = data_dict['email']
-        self.password = data_dict['password']
-        self.role = data_dict['role']
-        self.phone = data_dict['phone']
-        self.created_at = data_dict['created_at']
-        self.updated_at = data_dict['updated_at']
-        self.CIN = data_dict['CIN']
-        
+        self.id = data_dict["id"]
+        self.blood_type_id = ["blood_type_id"]
+        self.first_name = data_dict["first_name"]
+        self.last_name = data_dict["last_name"]
+        self.email = data_dict["email"]
+        self.password = data_dict["password"]
+        self.date_birth = data_dict["date_birth"]
+        self.role = data_dict["role"]
+        self.phone = data_dict["phone"]
+        self.created_at = data_dict["created_at"]
+        self.updated_at = data_dict["updated_at"]
+        self.CIN = data_dict["CIN"]
 
     @classmethod
     def create(cls, data_dict):
         query = """INSERT INTO users 
-                    (first_name, last_name, email, password,phone,address,CIN,date_birth,blood_type_id)
+                    (blood_type_id,first_name, last_name, email, password)
                     VALUES
-                    (%(first_name)s,%(last_name)s,%(email)s,%(password)s,%(phone)s,%(address)s,%(CIN)s,%(password)s,%(date_birth)s,%(blood_type_id)s);"""
+                    (1,%(first_name)s,%(last_name)s,%(email)s,%(password)s);"""
         print("***********not*********")
         return MySQLConnection(DATABASE_NAME).query_db(query, data_dict)
-    
+
     @classmethod
     def update(cls, data_dict):
         query = """UPDATE users SET
-                blood_type_id=%(blood_type_id)s, 
+                blood_type_id=%(blood_type_id)s,
+                gender=%(gender)s,
+                address=%(address)s,
+                date_birth=%(date_birth)s,
                 phone=%(phone)s, 
                 CIN=%(CIN)s
                 WHERE id=%(id)s"""
-        
+
         print("***********not*********")
         return MySQLConnection(DATABASE_NAME).query_db(query, data_dict)
-    
 
-    
     @classmethod
     def get_by_id(cls, data_dict):
         query = """SELECT * FROM users WHERE id =%(id)s;"""
         result = MySQLConnection(DATABASE_NAME).query_db(query, data_dict)
         return cls(result[0])
-    
 
     @classmethod
     def get_by_email(cls, data_dict):
         query = """SELECT * FROM users WHERE email = %(email)s;"""
-        result =MySQLConnection(DATABASE_NAME).query_db(query, data_dict)
+        result = MySQLConnection(DATABASE_NAME).query_db(query, data_dict)
         if result:
             return cls(result[0])
         return False
-    
-
-    
-
 
     @staticmethod
     def validate_register(data_dict):
         is_valid = True
-        if len( data_dict['first_name'])< 2:
+        if len(data_dict["first_name"]) < 2:
             print("First Name too short .....")
             flash("First Name too short .....", "first_name")
             is_valid = False
-        if len(data_dict['last_name'])< 2:
+        if len(data_dict["last_name"]) < 2:
             print("Last Name too short .....")
             flash("Last Name too short .....", "last_name")
             is_valid = False
-        if len(data_dict['password'])< 7:
+        if len(data_dict["password"]) < 7:
             print("Password too short .....")
             flash("Password too short .....", "password")
             is_valid = False
-        if data_dict['password'] != data_dict['confirm_password']:
+        if data_dict["password"] != data_dict["confirm_password"]:
             print("Password and Confirm password Don't match !!!!!")
-            flash("Password and Confirm password Don't match !!!!!", "confirm_password",)
+            flash(
+                "Password and Confirm password Don't match !!!!!",
+                "confirm_password",
+            )
             is_valid = False
-        if not EMAIL_REGEX.match(data_dict['email']): 
-            flash("Invalid email address!","email")
+        if not EMAIL_REGEX.match(data_dict["email"]):
+            flash("Invalid email address!", "email")
             is_valid = False
-        elif User.get_by_email({'email':data_dict['email']}):
+        elif User.get_by_email({"email": data_dict["email"]}):
             flash("Email Already taken . Hope by you !!!! ", "email")
             is_valid = False
         return is_valid
-
-
-   
